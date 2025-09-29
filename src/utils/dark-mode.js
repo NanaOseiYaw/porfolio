522291a112a57a965544625e9c8d302b06f2dc12
nana.osei <oseinanayaw28@gmail.com>
2025-09-29 15:20:45 +0200

    Working on making browser remember theme preference.

--- src/utils/dark-mode.js
+++ src/utils/dark-mode.js
@@ -1,1 +1,13 @@
-const themeToggleBtns = document.querySelectorAll('theme-toggle');
+const darkMode = () =>{
+
+    const themeToggleBtns = document.querySelectorAll('#theme-toggle');
+    
+    themeToggleBtns.forEach(btn => {
+        btn.addEventListener('click', () => {
+            document.body.classList.toggle('light-mode');
+            
+        });
+    });
+};
+
+export default darkMode;
