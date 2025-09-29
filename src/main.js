02adf9ff3b98a95dc2d3651d2e959302a5aef987
nana.osei <oseinanayaw28@gmail.com>
2025-09-29 12:48:43 +0200

    bezig met toggle dark and light mode and mobile nav

--- src/main.js
+++ src/main.js
@@ -3,4 +3,9 @@ import '../styles/style.css'
 import '../styles/components/header.css'
 import '../styles/components/hero.css'
 import '../styles/components/footer.css'
+import '../styles/components/mobile-nav.css'
 import '../styles/utils.css'
+
+import mobileNav from './utils/mobile-nav.js';
+
+mobileNav();
